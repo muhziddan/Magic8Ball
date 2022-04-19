f58ff4dff816d2c16e9eef3f9a3fb243f332243a
Muhammad Ziddan Hidayatullah <mziddanhidayatullah@gmail.com>
2022-04-19 12:16:47 +0700

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Muhammad Ziddan Hidayatullah on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
